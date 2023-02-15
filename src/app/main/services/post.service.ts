53c1315f9fa2f67d972bdcb7cba5302e4513fddf
Irving Didier <isarmientodelacruz@rccl.com>
2023-02-15 10:12:19 -0600

    add missing elemnts

---
+++ src/app/main/services/post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor() { }
+
+  getPost() {}
+
+  getPosts() {}
+
+  createPost() {}
+
+  updatePost() {}
+
+  deletePost() {}
+}
